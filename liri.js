63a5ab9195bd9987eaf02ab2c62006bd20847f4f
Maria <lrabin@gmail.com>
2019-05-30 16:10:24 -0400

    added venue for concerts

--- liri.js
+++ liri.js
@@ -26,17 +26,8 @@ function concert() {
     //node liri.js concert-this <artist/band name here>
     var axios = require("axios");
 
-    //This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-
     axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
-        //for (var i = 0; i < response.length; i++) {
         function (response) {
-            // Then we print out the 
-            //Name of the venue
-
-            //Venue location
-
-            //Date of the Event (use moment to format this as "MM/DD/YYYY")
             console.log("Name of the venue: " + response.data[0].venue.name);
             console.log("Venue of the location: " + response.data[0].venue.city + "," + response.data[0].venue.country);
             console.log("Date of the Event: " + response.data[0].datetime);
