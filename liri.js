02189431d797953dd7b8b67820d06875d83a7aa3
Maria <lrabin@gmail.com>
2019-05-31 22:00:28 -0400

    updated the movies & concert

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+
+var moment = require("moment");
 var action = process.argv[2];
 var value = process.argv[3];
 
@@ -22,34 +24,54 @@ switch (action) {
 var axios = require("axios");
 //Different functions below
 function concert() {
-    var axios = require("axios");
-    //node liri.js concert-this <artist/band name here>
-
-    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
-        function (response) {
-            console.log("Name of the venue: " + response.data[0].venue.name);
-            console.log("Venue of the location: " + response.data[0].venue.city + "," + response.data[0].venue.country);
-            console.log("Date of the Event: " + response.data[0].datetime);
-        })
+    if (value != undefined || value != null) {
+        var axios = require("axios");
+        //node liri.js concert-this <artist/band name here>
+        axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                var rawDate = response.data[0].datetime;
+                //var eventDate = moment.(rawDate).format('MM/DD/YYYY');
+                console.log("Name of the venue: " + response.data[0].venue.name);
+                console.log("Venue of the location: " + response.data[0].venue.city + "," + response.data[0].venue.country);
+                console.log("Date of the Event: " + moment(response.data[0].datetime).format('MM/DD/YYYY'));
+            });
+    }
+    if (value === undefined || value === '' || value === null) {
+        console.log("Pls. type a name of the band to search for.");
+    }
 };
+
 
 function movie() {
     var axios = require("axios");
+    var value = process.argv[3];
     //node liri.js movie-this <movie title>
-    axios.get("https://www.omdbapi.com/?t=" + value + "&apikey=trilogy").then(
-        function (response) {
-            console.log("Movie Title: " + response.data.Title);
-            console.log("Year the movie came out: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            console.log("Country where movie was produced: " + response.data.Country);
-            console.log("Movie Language: " + response.data.Year);
-            console.log("Movie Plot: " + response.data.Language);
-            console.log("Movie Actors: " + response.data.Actors);
-        });
-};
 
+    if (value === undefined || value === '' || value === null) {
+        axios.get("https://www.omdbapi.com/?t=Mr.Nobody&apikey=trilogy").then(
+            function (response) {
+                console.log("Movie Title: " + response.data.Title);
+                console.log("Year the movie came out: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Country where movie was produced: " + response.data.Country);
+                console.log("Movie Language: " + response.data.Year);
+                console.log("Movie Plot: " + response.data.Language);
+                console.log("Movie Actors: " + response.data.Actors);
+            })
+    };
 
-
-
-
+    if (value != undefined || value != null) {
+        axios.get("https://www.omdbapi.com/?t=Mr.Nobody&apikey=trilogy").then(
+            function (response) {
+                console.log("Movie Title: " + response.data.Title);
+                console.log("Year the movie came out: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Country where movie was produced: " + response.data.Country);
+                console.log("Movie Language: " + response.data.Year);
+                console.log("Movie Plot: " + response.data.Language);
+                console.log("Movie Actors: " + response.data.Actors);
+            })
+    };
+}
